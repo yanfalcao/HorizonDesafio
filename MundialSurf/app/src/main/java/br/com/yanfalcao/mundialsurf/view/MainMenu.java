3c62362c3e5e45925721694924216f541f65af0d
yanfalcao <yanfalcao10@gmail.com>
2019-08-31 04:06:48 -0300

    Melhorado o layout do menu principal com Swipe Views usando ViewPager

---
+++ MundialSurf/app/src/main/java/br/com/yanfalcao/mundialsurf/view/MainMenu.java
@@ -0,0 +1,107 @@
+package br.com.yanfalcao.mundialsurf.view;
+
+import android.animation.ArgbEvaluator;
+import android.content.Intent;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.com.yanfalcao.mundialsurf.R;
+import br.com.yanfalcao.mundialsurf.view.surfersViews.SurfersListActivity;
+
+public class MainMenu extends AppCompatActivity {
+
+    ViewPager viewPager;
+    PagerAdapter adapter;
+    List<Map<String, Object>> models;
+    Integer[] colors = null;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        modelInicialize();
+        colorsInicialize();
+
+        adapter = new MainMenuAdapter(models, this);
+
+        viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130, 0, 130, 0);
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+                if (i < (adapter.getCount() - 1) && i < (colors.length - 1)) {
+                    viewPager.setBackgroundColor(
+                            (Integer) argbEvaluator.evaluate(
+                                    v,
+                                    colors[i],
+                                    colors[i+1]));
+                }else {
+                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+    }
+
+    private void modelInicialize(){
+        Map<String, Object> item = new HashMap<>();
+        models = new ArrayList<>();
+
+        item.put("IMAGE", R.drawable.woman_surfer);
+        item.put("TITLE", "Surfers Register");
+        models.add(item);
+
+        item = new HashMap<>();
+        item.put("IMAGE", R.drawable.batterie);
+        item.put("TITLE", "Battery Register");
+        models.add(item);
+
+        item = new HashMap<>();
+        item.put("IMAGE", R.drawable.wave);
+        item.put("TITLE", "Wave Register");
+        models.add(item);
+
+        item = new HashMap<>();
+        item.put("IMAGE", R.drawable.punctuacion);
+        item.put("TITLE", "Score Register");
+        models.add(item);
+
+        item = new HashMap<>();
+        item.put("IMAGE", R.drawable.win);
+        item.put("TITLE", "Winner");
+        models.add(item);
+    }
+
+    private void colorsInicialize(){
+        Integer[] colors_helper = {
+                getResources().getColor(R.color.surfer),
+                getResources().getColor(R.color.battery),
+                getResources().getColor(R.color.wave),
+                getResources().getColor(R.color.score),
+                getResources().getColor(R.color.win)
+        };
+
+        colors = colors_helper;
+    }
+}
---
+++ MundialSurf/app/src/main/java/br/com/yanfalcao/mundialsurf/view/MainMenuAdapter.java
@@ -0,0 +1,92 @@
+package br.com.yanfalcao.mundialsurf.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+import br.com.yanfalcao.mundialsurf.R;
+import br.com.yanfalcao.mundialsurf.view.batteriesViews.NewBatteryActivity;
+import br.com.yanfalcao.mundialsurf.view.batteriesViews.winnerViews.BatteryListActivity;
+import br.com.yanfalcao.mundialsurf.view.notesViews.NewPunctuacionActivity;
+import br.com.yanfalcao.mundialsurf.view.surfersViews.SurfersListActivity;
+import br.com.yanfalcao.mundialsurf.view.wavesViews.NewWaveActivity;
+
+public class MainMenuAdapter extends PagerAdapter {
+
+    private List<Map<String, Object>> models;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public MainMenuAdapter(List<Map<String, Object>> models, Context context){
+        this.models = models;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return models.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view.equals(o);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull final ViewGroup container, final int position) {
+        layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_main_menu, container, false);
+
+        ImageView image = view.findViewById(R.id.image_main_menu);
+        TextView title = view.findViewById(R.id.title_main_menu);
+
+        image.setImageResource((int) models.get(position).get("IMAGE"));
+        title.setText((String) models.get(position).get("TITLE"));
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(position){
+                    case 0:
+                        context.startActivity(new Intent(context, SurfersListActivity.class));
+                        break;
+
+                    case 1:
+                        context.startActivity(new Intent(context, NewBatteryActivity.class));
+                        break;
+
+                    case 2:
+                        context.startActivity(new Intent(context, NewWaveActivity.class));
+                        break;
+
+                    case 3:
+                        context.startActivity(new Intent(context, NewPunctuacionActivity.class));
+                        break;
+
+                    case 4:
+                        context.startActivity(new Intent(context, BatteryListActivity.class));
+                        break;
+                }
+            }
+        });
+
+        container.addView(view, 0);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+}
